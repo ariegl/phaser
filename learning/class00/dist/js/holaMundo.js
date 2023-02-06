e88155f0b24310679adbec2c5b8842dde4a5dfe0
Ariel F <ariel.incgarcia@gmail.com>
2023-02-06 07:25:11 -0800

    first commit

---
+++ learning/class00/dist/js/holaMundo.js
@@ -0,0 +1,4 @@
+var mensaje = "Desarrollo de videojuegos con Phaser ";
+var version = 3;
+var mensaje2 = " Y TypeScript";
+console.log(mensaje + version + mensaje2);
---
+++ learning/class00/src/holaMundo.ts
@@ -0,0 +1,5 @@
+let mensaje : string = "Desarrollo de videojuegos con Phaser";
+let version : number = 3;
+let mensaje2 : string = " Y TypeScript";
+
+console.log(mensaje + version + mensaje2);
