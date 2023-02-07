fa659ab177c56f22d20ec48defe8537dbd330acd
Ariel F <ariel.incgarcia@gmail.com>
2023-02-06 19:37:41 -0800

    eventos

--- learning/class03/src/escenas/hud.ts
+++ learning/class03/src/escenas/hud.ts
@@ -1,22 +1,41 @@
-import { GameObjects } from "phaser";
-
 export default class HUD extends Phaser.Scene {
     private vidasTxt : Phaser.GameObjects.Text;
+    private puntuacionTxt : Phaser.GameObjects.Text;
 
-    constructor() {
+    private width: number;
+    private height: number;
+
+    constructor(){
         super('HUD');
     }
 
-    create() : void {
+    init(){
+        this.width = this.cameras.main.width;
+        this.height = this.cameras.main.height;
+    }
+
+    create(): void{
         const nivel1: Phaser.Scene = this.scene.get('Nivel1');
-        nivel1.events.on('cambiarvidas',this.actualizaVidas, this);
+        nivel1.events.on('cambiarvidas', this.actualizaVidas, this);
+        nivel1.events.on('cambiaPuntuacion', this.actualizaPuntuacion, this);
 
-        this.vidasTxt = this.add.text(20,20,"Vidas: 3", {fontSize: '32px',color: '#FFFFFF'});
+
+
+        this.vidasTxt = this.add.text(20,20, 'Vidas:3', {fontSize:'32px', color:'#FFFFFF'});
         
+        this.puntuacionTxt = this.add.text(this.width - 50 ,20, '000', { fontSize: '20px', color: '#FFFFFF' })
+
+
     }
 
-    private actualizaVidas() : void {
-        this.vidasTxt.text = "Vidas: " + this.registry.get('vidas');
+    private actualizaVidas(): void{
+        this.vidasTxt.text = "Vidas:" + this.registry.get('vidas');
     }
+
+    private actualizaPuntuacion(): void {
+        this.puntuacionTxt.text = Phaser.Utils.String.Pad(this.registry.get("puntuacion"), 3, '0', 1);
+    }
+
+
 
 }
--- learning/class03/src/escenas/nivel1.ts
+++ learning/class03/src/escenas/nivel1.ts
@@ -1,8 +1,11 @@
 export default class Nivel1 extends Phaser.Scene
 {
-    private width : number;
-    private height : number;
-    private vidas : number;
+    private width: number;
+    private height: number;
+
+    private vidas: number;
+    private puntuacion: number;
+
 
     constructor ()
     {
@@ -12,25 +15,40 @@ export default class Nivel1 extends Phaser.Scene
     init(){
         this.width = this.cameras.main.width;
         this.height = this.cameras.main.height;
+
         this.vidas = 3;
+        this.puntuacion = 0;
     }
+
 
     preload ()
     {
         
     }
-    
 
     create ()
     {        
         const logo = this.add.image(400, 70, 'logo1');
-        const jugarTxt : Phaser.GameObjects.Text = this.add.text(50,this.height / 2, "NIVEL 1", {fontSize: '32px', color: '#FFFFFF'});
-        const vidasTxt : Phaser.GameObjects.Text = this.add.text(this.width / 2,this.height / 2, "Vidas -", {fontSize: '32px', color: '#FFFFFF'}).setInteractive();
-  
-        vidasTxt.on("pointerdown", () => {
+        
+        const jugarTxt: Phaser.GameObjects.Text = this.add.text(50, this.height/2, 'NIVEL 1', {fontSize:'32px', color:'#FFFFFF'});
+        
+        const vidasTxt: Phaser.GameObjects.Text = this.add.text(this.width/2, this.height/2, 'VIDAS -', {fontSize:'32px', color:'#FFFFFF'}).setInteractive();
+
+        vidasTxt.on('pointerdown', ()=>{
             this.vidas --;
-            this.registry.set('vidas',this.vidas);
+            this.registry.set('vidas', this.vidas);
             this.events.emit('cambiarvidas');
         });
+
+        const puntuacionTxt: Phaser.GameObjects.Text  = this.add.text(this.width/2  , this.height/2 + 100 , 'Puntuacion',  { fontSize: '32px', color: '#FFFFFF' })
+                                                .setInteractive();         
+                                                        
+        puntuacionTxt.on('pointerdown', () => {                                                                    
+            this.puntuacion++;
+            this.registry.set('puntuacion', this.puntuacion);
+            this.events.emit('cambiaPuntuacion');
+        });
+
+
     }
 }
